9272f696b8ee62eedab49d384b67e635e54c6054
Josh Lando <54454202+j4lando@users.noreply.github.com>
2025-01-16 13:00:34 -0800

    updated home page to new style

--- home-script.js
+++ home-script.js
@@ -1,11 +1,19 @@
-document.getElementById('startDesigning').addEventListener('click', () => {
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (currentScrollTop > lastScrollTop) {
+        // Scrolling down
+        document.getElementById("header").style.top = "-100px";
+    } else {
+        // Scrolling up
+        document.getElementById("header").style.top = "0";
+    }
+    
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+}, false);
+
+document.getElementById('build-button').addEventListener('click', () => {
     window.location.href = 'building.html';
-});
-
-document.getElementById('signIn').addEventListener('click', () => {
-    // Implement sign in functionality
-});
-
-document.getElementById('signUp').addEventListener('click', () => {
-    // Implement sign up functionality
 });
